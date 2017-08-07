52c5bea5097513d73360f47f39646c357d0f764e
ryan-vubt <xinshirn@163.com>
2017-08-07 17:57:02 +0800

    update Android_APIs.pdf

--- api2pdf_v1.py
+++ api2pdf_v1.py
@@ -12,16 +12,33 @@ title=pg.xpath('//div[@class="nav-section-header"]/a/@href')
 
 lis=pg.xpath('//li[@class="nav-section"]')
 
-fp=open('url1.txt', 'a+')
+fp=open('url2.txt', 'a+')
 
 for li in lis:
 	li_h_url=li.xpath('div/a/@href')[0]
 	li_h_nam=li.xpath('div/a/text()')[0]
-	fp.write(li_h_url+'\n')
+	fp.write((li_h_url+','+li_h_nam+'\n').encode('utf-8'))
 	#fp.write(li_h_nam+'\n')
 	for li_b in li.xpath('ul/li/a'):
 		li_b_url=li_b.xpath('@href')[0]
 		li_b_nam=li_b.xpath('text()')[0]
-		fp.write(li_b_url+'\n')
+		fp.write((li_b_url+','+li_b_nam+'\n').encode('utf-8'))
 		#fp.write(li_b_nam+'\n')
-	fp.write('\n')
+
+
+#for i,s in enumerate(d):
+#  d[i]=s.rstrip('>')
+#
+#for i,s in enumerate(d):
+#  d[i]=s.lstrip('<')
+#
+#for i,s in enumerate(d):
+#  d[i]=s.replace('/','')
+#
+#for i in os.listdir():
+# if (re.match('A([0-9]+)_.pdf',i)):
+#  id=re.search('A([0-9]+)_.pdf',i).group(1)
+#  try:
+#   os.rename(i,'A'+id+'_'+d[int(id)]+'.pdf')
+#  except:
+#   print(i)
